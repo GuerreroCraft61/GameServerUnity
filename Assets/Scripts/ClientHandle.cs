ce13839507e4f85a49d6e7de40ac5d1d5add057b
GuerreroCraft61 <guerrerocraft61@gmail.com>
2020-12-05 16:49:43 +0100

    Initial Commit

---
+++ Assets/Scripts/ClientHandle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientHandle : MonoBehaviour {
+
+	public static void Welcome(Packet packet) {
+		string msg = packet.ReadString();
+		int myId = packet.ReadInt();
+		
+		Debug.Log($"Message from server: {msg}");
+		Client.instance.myId = myId;
+		ClientSend.WelcomeReceived();
+	}
+}
